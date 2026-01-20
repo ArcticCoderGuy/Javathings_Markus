6ad1575124919cc2b9ac3df187e7447f04d987ee
NorthFoxAI <markus.kaprio@edunord.se>
2026-01-20 10:05:21 +0100

    Upadated the README-file

--- Vojker.java
+++ Vojker.java
@@ -1,6 +1,7 @@
 public static class Vojker {
     public static class main(String [] args ) {
 
-     Bool thisIsH1Timeframe =  60;  
+     Bool thisIsH1Timeframe =  60; 
+     System.out.println("") 
     }
 }
